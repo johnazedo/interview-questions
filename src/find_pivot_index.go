b2e61441f6d15e82c29962bc3e5d248be5f3dabb
João Pedro Limão <johnazedo07@gmail.com>
2022-12-05 19:46:56 -0300

    Resolve 2 questions

--- src/find_pivot_index.go
+++ src/find_pivot_index.go
@@ -1,5 +1,24 @@
 package src
 
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/find-pivot-index
+*/
 func pivotIndex(nums []int) int {
-	return 0
+	mount := 0 // O(1)
+	for i := 0; i < len(nums); i++ {
+		if i > 0 {
+			mount += nums[i-1] // O(N)
+		}
+		sum := 0
+
+		for j := i + 1; j < len(nums); j++ {
+			sum += nums[j] // O(N^2)
+		}
+		if sum == mount { // O(N)
+			return i // O(1)
+		}
+	}
+
+	return -1
 }
--- src/running_sum_of_1d_array.go
+++ src/running_sum_of_1d_array.go
@@ -7,6 +7,7 @@ Link: https://leetcode.com/problems/running-sum-of-1d-array
 func runningSum(nums []int) []int {
 	prefix := 0
 
+	// O(N)
 	for i, n := range nums {
 		prefix += n
 		nums[i] = prefix
