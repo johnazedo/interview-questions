8d1af7ff5b0fe562c86176f9176546c9129d4a58
João Pedro Limão <johnazedo07@gmail.com>
2022-12-06 18:41:14 -0300

    Resolve isomorphic_strings problem

--- src/isomorphic_strings.go
+++ src/isomorphic_strings.go
@@ -1,1 +1,20 @@
 package src
+
+func isIsomorphic(s string, t string) bool {
+	stot := make(map[byte]byte)
+	ttos := make(map[byte]byte)
+
+	for i := 0; i < len(s); i++ {
+		sValue, sInT := stot[s[i]]
+		tValue, tInS := ttos[t[i]]
+
+		if !sInT && !tInS {
+			stot[s[i]] = t[i]
+			ttos[t[i]] = s[i]
+		} else if sValue != t[i] || tValue != s[i] {
+			return false
+		}
+	}
+
+	return true
+}
