a42cf1ac988c3524a31f12872d4f959a3724efed
João Pedro Limão <johnazedo07@gmail.com>
2022-12-07 15:57:23 -0300

    Resolve merge_two_sorted_list problem

---
+++ src/merge_two_sorted_list.go
@@ -0,0 +1,36 @@
+package src
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	head := &ListNode{}
+	var v1 int
+	var v2 int
+	temp := head
+	for list1 != nil || list2 != nil {
+		if list1 != nil {
+			v1 = list1.Val
+		} else {
+			v1 = 999
+		}
+		if list2 != nil {
+			v2 = list2.Val
+		} else {
+			v2 = 999
+		}
+
+		if v1 <= v2 {
+			temp.Next = list1
+			list1 = list1.Next
+		} else {
+			temp.Next = list2
+			list2 = list2.Next
+		}
+		temp = temp.Next
+
+	}
+	return head.Next
+}
---
+++ src/reverse_linked_list.go
@@ -0,0 +1,5 @@
+package src
+
+func reverseList(head *ListNode) *ListNode {
+	return head
+}
