0e5336df7bf4fa78f41cfb567f381dec26e7d9f3
João Pedro Limão <johnazedo07@gmail.com>
2022-12-17 15:24:07 -0300

    Reverse a linked list

--- src/reverse_linked_list.go
+++ src/reverse_linked_list.go
@@ -1,5 +1,20 @@
 package src
 
 func reverseList(head *ListNode) *ListNode {
+	stack := []int{}
+	temp := head
+
+	for temp != nil {
+		stack = append(stack, temp.Val)
+		temp = temp.Next
+	}
+
+	temp = head
+
+	for i := len(stack) - 1; i >= 0; i-- {
+		temp.Val = stack[i]
+		temp = temp.Next
+	}
+
 	return head
 }
