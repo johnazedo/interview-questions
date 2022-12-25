18a279576cd61c360c3206b2070b5490f0f70e1c
João Pedro Limão <johnazedo07@gmail.com>
2022-12-25 12:48:47 -0300

    Resolve nary_tree_preorder_traversal problem

--- src/data_structures.go
+++ src/data_structures.go
@@ -4,3 +4,8 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
---
+++ src/easy/nary_tree_preorder_tranversal.go
@@ -0,0 +1,29 @@
+package easy
+
+import (
+	. "github.com/johnazedo/leetcode/src"
+)
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/n-ary-tree-preorder-traversal
+*/
+
+func preorder(root *Node) []int {
+	result := make([]int, 0)
+	stack := make([]*Node, 0)
+
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		n := len(stack) - 1
+		root = stack[n]
+		if root != nil {
+			result = append(result, root.Val)
+			stack = stack[:n]
+			for i := len(root.Children) - 1; i >= 0; i-- {
+				stack = append(stack, root.Children[i])
+			}
+		}
+	}
+	return result
+}
