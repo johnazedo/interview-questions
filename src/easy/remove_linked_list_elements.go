9972e1c358226dde4e793a1ad2d04516c74ba694
João Pedro Limão <johnazedo07@gmail.com>
2022-12-26 09:27:31 -0300

    Resolve remove_linked_list_elements problem

---
+++ src/easy/remove_linked_list_elements.go
@@ -0,0 +1,34 @@
+package easy
+
+import (
+	. "github.com/johnazedo/leetcode/src"
+)
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/remove-linked-list-elements/
+*/
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+	// Time: O(N)
+	// Space: O(1)
+	var prevNode *ListNode
+	temp := head
+	for temp != nil {
+		if val == temp.Val {
+			if prevNode != nil {
+				prevNode.Next = temp.Next
+				temp = temp.Next
+			} else {
+				head = head.Next
+				temp = head
+			}
+		} else {
+			prevNode = temp
+			temp = temp.Next
+		}
+	}
+	return head
+}
