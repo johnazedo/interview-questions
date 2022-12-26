21c841767301ca3939d7e94e455cbdfb2c689822
João Pedro Limão <johnazedo07@gmail.com>
2022-12-26 09:44:35 -0300

    Resolve binary_search problem

---
+++ src/easy/binary_search.go
@@ -0,0 +1,26 @@
+package easy
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/binary-search/
+*/
+func search(nums []int, target int) int {
+	// Time: O(LogN)
+	// Space: O(1)
+	start := 0
+	end := len(nums) - 1
+	var pivot int
+	for start <= end {
+		pivot = start + (end-start)/2
+		if nums[pivot] == target {
+			return pivot
+		}
+		if nums[pivot] < target {
+			start = pivot + 1
+		}
+		if nums[pivot] > target {
+			end = pivot - 1
+		}
+	}
+	return -1
+}
