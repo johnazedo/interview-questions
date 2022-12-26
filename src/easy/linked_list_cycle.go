d6dc11845022259271a401e90bc4aa7abd806a1c
João Pedro Limão <johnazedo07@gmail.com>
2022-12-26 08:54:54 -0300

    Resolve linked_list_cycle problem

---
+++ src/easy/linked_list_cycle.go
@@ -0,0 +1,30 @@
+package easy
+
+import (
+	. "github.com/johnazedo/leetcode/src"
+)
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/linked-list-cycle/
+*/
+func hasCycle(head *ListNode) bool {
+
+	// TODO: Do this with O(1) space complexity
+	// Time: O(N)
+	// Space: O(N)
+	mapping := make(map[*ListNode]int)
+	if head == nil {
+		return false
+	}
+
+	for head != nil {
+		if _, ok := mapping[head]; ok {
+			return true
+		}
+		mapping[head] = 0
+		head = head.Next
+	}
+
+	return false
+}
