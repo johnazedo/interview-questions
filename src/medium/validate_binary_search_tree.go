d0f89ef3143a34867bc0244c863b95af89ee0a3b
João Pedro Limão <johnazedo07@gmail.com>
2022-12-27 12:50:38 -0300

    Resolve validate_binary_search_tree problem

---
+++ src/medium/validate_binary_search_tree.go
@@ -0,0 +1,31 @@
+package medium
+
+import (
+	. "github.com/johnazedo/leetcode/src"
+	"math"
+)
+
+/*
+Difficult: Medium
+Link: https://leetcode.com/problems/validate-binary-search-tree/
+*/
+func isValidBST(root *TreeNode) bool {
+	min := math.MinInt
+	max := math.MaxInt
+	// Time: ??
+	// Space: ??
+	return helper(root, min, max)
+}
+
+func helper(root *TreeNode, min int, max int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val < min || root.Val > max {
+		return false
+	}
+
+	return helper(root.Left, min, root.Val-1) &&
+		helper(root.Right, root.Val+1, max)
+}
