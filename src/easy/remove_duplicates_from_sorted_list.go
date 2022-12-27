7e4fd1e7eb642763ea54eb02154e26d7b7c52b1f
João Pedro Limão <johnazedo07@gmail.com>
2022-12-27 08:39:46 -0300

    Resolve remove_duplicates_from_sorted_list problem

---
+++ src/easy/remove_duplicates_from_sorted_list.go
@@ -0,0 +1,25 @@
+package easy
+
+import . "github.com/johnazedo/leetcode/src"
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	temp := head
+
+	// Time: O(N)
+	// Space: O(1)
+	for temp != nil && temp.Next != nil {
+		if temp.Val == temp.Next.Val {
+			temp.Next = temp.Next.Next
+		} else {
+			temp = temp.Next
+		}
+	}
+	return head
+}
