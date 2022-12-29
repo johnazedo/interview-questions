11940b663bf8564d2ef325a6d32b7589c145a9ce
João Pedro Limão <johnazedo07@gmail.com>
2022-12-29 10:20:09 -0300

    Resolve binary_tree_postorder_traversal problem

---
+++ src/easy/binary_tree_postorder_traversal.go
@@ -0,0 +1,54 @@
+package easy
+
+import (
+	. "github.com/johnazedo/leetcode/src"
+)
+
+/*
+Difficult: Easy
+Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+*/
+func postorderTraversal(root *TreeNode) []int {
+	// N is the number of nodes and H is the height of the tree
+	// Time: O(N)
+	// Space: O(H)
+	var result []int
+	recursivePost(root, &result)
+	return result
+}
+
+func recursivePost(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
+	recursivePost(root.Left, result)
+	recursivePost(root.Right, result)
+	*result = append(*result, root.Val)
+}
+
+// This is not working
+// Error: out of memory (infinite loop)
+// Solution: Use a right child stack
+func iterativePost(root *TreeNode) []int {
+	var stack []*TreeNode
+	var result []int
+	var lastNode *TreeNode
+
+	for len(stack) > 0 || root != nil {
+		if root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		} else {
+			n := len(stack) - 1
+			peekNode := stack[n]
+			if peekNode.Right != nil && lastNode != peekNode.Right {
+				root = peekNode.Right
+			} else {
+				stack = stack[:n]
+				result = append(result, peekNode.Val)
+				lastNode = root
+			}
+		}
+	}
+	return result
+}
