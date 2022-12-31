4325106860fd8946d9ac5ac94ca0fd83c0817b5e
João Pedro Limão <johnazedo07@gmail.com>
2022-12-31 11:30:57 -0300

    Resolve invert_binary_tree problem

---
+++ src/easy/invert_binary_tree.go
@@ -0,0 +1,21 @@
+package easy
+
+import . "github.com/johnazedo/leetcode/src"
+
+/*
+Difficult: Easy
+LinK: https://leetcode.com/problems/invert-binary-tree/
+*/
+func invertTree(root *TreeNode) *TreeNode {
+	// TODO: Try this iteratively
+	// Time: O(N)
+	// Space: O(H)
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
