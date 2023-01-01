86dc1ef97972820954190decee399a40770de45f
João Pedro Limão <johnazedo07@gmail.com>
2023-01-01 13:18:05 -0300

    Resolve insert_into_a_binary_search_tree problem

---
+++ src/medium/insert_into_a_binary_search_tree.go
@@ -0,0 +1,36 @@
+package medium
+
+import . "github.com/johnazedo/leetcode/src"
+
+/*
+Difficult: Medium
+Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+*/
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	// TODO: Can create this recursively?
+	// Time: O(Log(N))
+	// Space: O(1)
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	curr := root
+	for curr != nil {
+		if curr.Val < val {
+			if curr.Right != nil {
+				curr = curr.Right
+			} else {
+				curr.Right = &TreeNode{Val: val}
+				break
+			}
+		} else {
+			if curr.Left != nil {
+				curr = curr.Left
+			} else {
+				curr.Left = &TreeNode{Val: val}
+				break
+			}
+		}
+	}
+	return root
+}
