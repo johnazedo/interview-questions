a25546f697ca8102dd5e1a0d7c803a5780270114
João Pedro Limão <jplimao12@gmail.com>
2023-01-17 11:46:13 -0300

    Reviewed invert_binary_tree solution

--- leetcode/easy/invert_binary_tree.go
+++ leetcode/easy/invert_binary_tree.go
@@ -3,11 +3,14 @@ package easy
 import . "github.com/johnazedo/interview-questions/leetcode"
 
 /*
+Number 226
 Difficult: Easy
 LinK: https://leetcode.com/problems/invert-binary-tree/
+Tags: Tree, Depth-First Search, Breadth-First Search, Binary Tree
+Status: Reviewed
 */
 func invertTree(root *TreeNode) *TreeNode {
-	// TODO: Try this iteratively
+	// Recursive solution
 	// Time: O(N)
 	// Space: O(H)
 	if root == nil {
@@ -17,5 +20,30 @@ func invertTree(root *TreeNode) *TreeNode {
 	root.Left, root.Right = root.Right, root.Left
 	invertTree(root.Left)
 	invertTree(root.Right)
+	return root
+}
+
+func invertTreeIT(root *TreeNode) *TreeNode {
+	// Iterative solution
+	// Time: O(N)
+	// Space: O(H)
+
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		n := len(stack) - 1
+		node := stack[n]
+		stack = stack[:n]
+		if node != nil {
+			node.Left, node.Right = node.Right, node.Left
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+		}
+	}
+
 	return root
 }
