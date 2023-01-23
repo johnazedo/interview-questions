1f3714ae5590eea636fe0f14a02fc1abef5fe516
João Pedro Limão <johnazedo07@gmail.com>
2023-01-23 09:45:22 -0300

    Review first_unique_character_in_a_string problem

--- leetcode/easy/first_unique_character_in_a_string.go
+++ leetcode/easy/first_unique_character_in_a_string.go
@@ -5,31 +5,21 @@ Number: 387
 Difficult: Easy
 Link: https://leetcode.com/problems/first-unique-character-in-a-string/
 Tags: Hash Table, String, Queue
-Status: Resolved
+Status: Reviewed
 */
 func firstUniqChar(s string) int {
-	// TODO: Review this question
 	// Time: O(N)
-	// Space: O(N)
-	mapping := make(map[int32]int)
+	// Space: O(1) -> Because there are only 26 letters in english alphabet
+	mapping := make(map[rune]int)
+	for _, c := range s {
+		mapping[c] = mapping[c] + 1
+	}
+
 	for i, c := range s {
-		if _, ok := mapping[c]; ok {
-			mapping[c] = -1
-			continue
-		}
-		mapping[c] = i
-	}
-
-	min := 100000
-	for _, v := range mapping {
-		if v < min && v != -1 {
-			min = v
+		if mapping[c] == 1 {
+			return i
 		}
 	}
 
-	if min == 100000 {
-		return -1
-	}
-
-	return min
+	return -1
 }
