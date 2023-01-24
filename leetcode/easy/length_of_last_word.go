9b9a388240c16e4bbd61e826caa44bdfa2f8bb44
João Pedro Limão <johnazedo07@gmail.com>
2023-01-24 09:45:36 -0300

    Review lenght_of_last_word problem

--- leetcode/easy/length_of_last_word.go
+++ leetcode/easy/length_of_last_word.go
@@ -5,15 +5,18 @@ import (
 )
 
 /*
+Number: 58
 Difficult: Easy
 Link: https://leetcode.com/problems/length-of-last-word/
+Tags: String
+Status: Reviewed
 */
 func lengthOfLastWord(s string) int {
 	s = strings.Trim(s, " ")
 	lastSpace := -1
 
 	// Time: O(N)
-	// Space: O(1)
+	// Space: O(N) -> Because Trim makes a copy of the original string
 	for i := len(s) - 1; i >= 0; i-- {
 		if string(s[i]) == " " {
 			lastSpace = i
@@ -22,4 +25,26 @@ func lengthOfLastWord(s string) int {
 	}
 
 	return len(s[lastSpace+1:])
+}
+
+// One loop approach
+func lengthOfLastWordBetterSpaceComplexity(s string) int {
+	// Time: O(N)
+	// Space: O(1)
+
+	pointer := len(s) - 1
+	length := 0
+
+	for pointer >= 0 {
+
+		if string(s[pointer]) != " " {
+			length++
+		} else if length > 0 {
+			break
+		}
+
+		pointer--
+	}
+
+	return length
 }
