4c479da113307bb5508b007a5edbee4d00f5e894
João Pedro Limão <johnazedo07@gmail.com>
2023-01-25 07:56:50 -0300

    Review longest_palindrome problem

--- leetcode/easy/longest_palindrome.go
+++ leetcode/easy/longest_palindrome.go
@@ -1,22 +1,30 @@
 package easy
 
 /*
+Number: 409
 Difficult: Easy
 Link: https://leetcode.com/problems/longest-palindrome/
+Tags: Hash Table, String, Greedy
+Status: Reviewed
 */
 func longestPalindrome(s string) int {
-	mapping := make(map[string]int)
+	// Time: O(N)
+	// Space: O(1) -> Because there are only 26 letters in English alphabet
+	mapping := make(map[rune]int)
 	sum := 0
 	for _, c := range s {
-		key := string(c)
-		if _, ok := mapping[key]; ok {
+		// When found a duplicate letter, add 2 to counter and remove this from map.
+		if _, ok := mapping[c]; ok {
 			sum += 2
-			delete(mapping, key)
+			delete(mapping, c)
 		} else {
-			mapping[key] = 1
+			mapping[c] = 1
 		}
 	}
 
+	// If the size of map is grater than 0 it means that
+	// you can choose one letter to put in middle of palindrome,
+	// so add 1 to counter.
 	if len(mapping) > 0 {
 		sum++
 	}
