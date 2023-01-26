18acf6aaea1f01abb2d517977d287b8f30ac5d17
João Pedro Limão <johnazedo07@gmail.com>
2023-01-26 08:35:53 -0300

    Resolved maximun_subarray problem

--- leetcode/medium/maximum_subarray.go
+++ leetcode/medium/maximum_subarray.go
@@ -1,12 +1,18 @@
 package medium
 
 /*
+Number: 53
 Difficult: Medium
 Link: https://leetcode.com/problems/maximum-subarray/
+Tags: Array, Divider and Conquer, Dynamic Programming
+Status: Resolved
 */
+
+// Brute force approach (not accepted)
 func maxSubArray(nums []int) int {
 	total := 0
 	max := -9999999
+	// TODO: Do this with divide to conquer approach
 
 	// Time: O(N^2)
 	// Space: O(1)
@@ -23,4 +29,24 @@ func maxSubArray(nums []int) int {
 		}
 	}
 	return max
+}
+
+// Dynamic programming approach
+func maxSubArrayDP(nums []int) int {
+	// Time: O(N)
+	// Space: O(1)
+	maxSub := nums[0]
+	currentSub := 0
+
+	for _, n := range nums {
+		currentSub += n
+		if currentSub > maxSub {
+			maxSub = currentSub
+		}
+
+		if currentSub < 0 {
+			currentSub = 0
+		}
+	}
+	return maxSub
 }
